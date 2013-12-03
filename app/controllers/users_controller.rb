cf3d4046febb7bed85a09cc91f55a069f501ef56
Peter <psmir777@gmail.com>
2013-12-03 17:58:45 +0200

    refactor users controller and spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,18 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user! 
+  before_filter :authenticate_user!
   before_filter :find_blogger
 
   def follow
-    current_user.follow(@blogger)
+    current_user.follow @blogger
     redirect_to user_articles_path(@blogger)
   end
- 
+
   def unfollow
     current_user.unfollow @blogger
     redirect_to user_articles_path(@blogger)
   end
 
   def find_blogger
-    @blogger = User.find params[:id]
-
-    if @blogger.nil?
-      flash[:alert] = 'The user was not found'
-      redirect_to root_path
-    end
+    @blogger = User.find(params[:id])
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,88 +2,30 @@ require 'spec_helper'
 
 describe UsersController do
 
-  shared_context '#follow/#unfollow common context' do
-    include_context 'authenticated user'   
- 
-    before do
-      User.stub(:find).with('1').and_return @blogger = stub('blogger')
-    end
+  include_context 'authenticated user'
+
+  before do
+    User.stub(:find).with('1').and_return @blogger = stub('blogger')
   end
 
-  shared_examples 'wrong blogger id handler' do 
+
+  describe '#follow' do
 
     before do
-      User.should_receive(:find).with('1').and_return nil
-      do_request
+      @current_user.should_receive(:follow).with @blogger
+      post :follow, id: 1
     end
 
-    it 'sets a message that the blogger was not found' do
-      flash[:alert].should == 'The user was not found'
-    end
-
-    it 'redirects to the root' do
-      response.should redirect_to root_path
-    end
+    it { should redirect_to user_articles_path(@blogger) }
   end
 
-  describe 'POST #follow' do
-    include_context '#follow/#unfollow common context'
+  describe '#unfollow' do
+
     before do
-      @current_user.stub(:follow)
-    end
- 
-    def do_request(args = {})
-      post :follow, { id: 1 }.merge(args)
+      @current_user.should_receive(:unfollow).with @blogger
+      post :unfollow, id: 1
     end
 
-    it_behaves_like 'requiring authentication'
-
-    it 'finds the blogger' do
-      do_request
-      assigns[:blogger].should == @blogger
-    end
-
-    it "adds the blogger to user's list of favorite bloggers" do
-      @current_user.should_receive(:follow).with(@blogger)
-      do_request
-    end
-  
-    it "redirects to the blogger's blog" do
-      do_request
-      response.should redirect_to user_articles_path(@blogger)
-    end
-
-    it_behaves_like 'wrong blogger id handler'
+    it { should redirect_to user_articles_path(@blogger) }
   end
-
-  describe 'POST #unfollow' do
-    include_context '#follow/#unfollow common context'
-    before do
-      @current_user.stub(:unfollow)
-    end
-
-    def do_request(args = {})
-      post :unfollow, { id: 1 }.merge(args)
-    end
-
-    it_behaves_like 'requiring authentication'
-
-    it 'finds the blogger' do
-      do_request
-      assigns[:blogger].should == @blogger
-    end
-
-    it "adds the blogger to the user's list of favorite bloggers" do
-      @current_user.should_receive(:unfollow).with(@blogger)
-      do_request
-    end
-  
-    it "redirects to the blogger's blog" do
-      do_request
-      response.should redirect_to user_articles_path(@blogger)
-    end
-
-    it_behaves_like 'wrong blogger id handler'
-  end
-
 end
